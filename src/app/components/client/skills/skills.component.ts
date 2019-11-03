5ee04d400e1eadcf19724638eae2aab3412d13a7
Adnene HAMDOUNI <adnene.hamdouni@qugenx.com>
2019-11-03 13:06:51 +0100

    Add Session storage to save and retreive data from Skills Component

--- src/app/components/client/skills/skills.component.ts
+++ src/app/components/client/skills/skills.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from "@angular/core";
 import { SkillsService } from "src/app/services/skills.service";
 import { Skills } from "src/app/model/skills.model";
 import { SkillsResponse } from "src/app/core/responses/skills-response.model";
+import { isStorageAvailable } from "ngx-webstorage-service";
+import { SessionStorageService } from "src/app/shared/services/storage/session-storage.service";
 
 @Component({
   selector: "app-skills",
@@ -13,13 +15,30 @@ export class SkillsComponent implements OnInit {
   skillss: Skills[];
   response: SkillsResponse;
 
-  constructor(private skillsServie: SkillsService) {}
+  constructor(
+    private skillsServie: SkillsService,
+    private sessionStorageService: SessionStorageService
+  ) {}
 
   public hasSkillss: boolean = false;
 
   ngOnInit() {
     console.log("SkillsComponent: ngOnInit");
+    const sessionStorageAvailable = isStorageAvailable(sessionStorage);
+
+    this.trySaveSessionStrogage();
+    this.tryLoadSessionStrogage();
+
+    console.log(`Session storage available: ${sessionStorageAvailable}`);
     this.getSkills();
+  }
+
+  trySaveSessionStrogage() {
+    this.sessionStorageService.setToLocal();
+  }
+
+  tryLoadSessionStrogage() {
+    this.sessionStorageService.getFromLocal("pure-awesomeness");
   }
 
   getSkills() {
--- src/app/shared/services/storage/session-storage.service.ts
+++ src/app/shared/services/storage/session-storage.service.ts
@@ -5,7 +5,7 @@ export const MY_AWESOME_SERVICE_STORAGE = new InjectionToken<StorageService>(
   "MY_AWESOME_SERVICE_STORAGE"
 );
 
-const STORAGE_KEY = "pure-awesomeness";
+const STORAGE_KEY: string = "pure-awesomeness";
 
 @Injectable({
   providedIn: "root"
@@ -17,9 +17,28 @@ export class SessionStorageService {
     @Inject(MY_AWESOME_SERVICE_STORAGE) private storage: StorageService
   ) {}
 
-  public doSomethingAwesome(): number {
+  public setToLocal(): number {
     const awesomenessLevel: number = this.storage.get(STORAGE_KEY) || 1337;
+
+    const valueTosTore: number = awesomenessLevel + 1;
+
+    console.log(
+      "SessionStorageService: getFromLocal -> STORAGE_KEY = " +
+        STORAGE_KEY +
+        " and data to store = " +
+        valueTosTore
+    );
     this.storage.set(STORAGE_KEY, awesomenessLevel + 1);
     return awesomenessLevel;
+  }
+
+  public getFromLocal(key: string): any {
+    console.log("recieved= key:" + key);
+    const value: number = this.storage.get(key);
+    //this.data[key] = this.storage.get(key);
+    console.log(
+      "SessionStorageService: getFromLocal -> value = " + JSON.stringify(value)
+    );
+    return value;
   }
 }
