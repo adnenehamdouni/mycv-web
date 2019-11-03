95ecd7374d5b800af60a49b0f7068fe1136d04f0
Adnene HAMDOUNI <adnene.hamdouni@qugenx.com>
2019-11-03 11:42:57 +0100

    Add header, footer and layout container. move nav bar to header, and router-outlet to layout container

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,12 @@ import { HttpClientModule } from "@angular/common/http";
 import { AppComponent } from "./app.component";
 import { HomeComponent } from "./components/client/home/home.component";
 import { SkillsComponent } from "./components/client/skills/skills.component";
+import { FooterComponent } from './shared/footer/footer.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { LayoutComponent } from './shared/layout/layout.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, SkillsComponent],
+  declarations: [AppComponent, HomeComponent, SkillsComponent, FooterComponent, HeaderComponent, LayoutComponent],
   imports: [BrowserModule, FormsModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent]
